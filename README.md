6a7bdcb8219558a93769eefbc6a2956a46fd3c9e
haxaru <Sammyalanis43@gmail.com>
2023-12-19 02:09:26 -0700

    Add initial console version

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# tic-tac-toe
+# Tic-Tac-Toe: Console Edition
+
+This is a console-based version of Tic-Tac-Toe implemented in JavaScript. Please note that the display will be added in later commits.
+
+## Overview
+
+This game is a straightforward implementation of the classic Tic-Tac-Toe. Players take turns marking their positions on a 3x3 grid, aiming to get three of their marks in a row - horizontally, vertically, or diagonally.
+
+## How to Play
+
+- **Invoke the Function**: Use `GameControls.playRound(position)` to make a move.
+- **Positioning**: The game board positions range from 0 (top left) to 8 (bottom right).
+- **Players**: Currently designed for two players. Players take turns marking their positions.
+
+## Future Updates
+
+- **AI Player**: A future version aims to incorporate an AI opponent.
+- **Display**: Display implementation will be added in upcoming commits.
+
+## Usage
+
+```javascript
+// Example: Play a round by marking position 4
+GameControls.playRound(4);
+```
--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
-const playerManagement = (function (
+const PlayerManager = (function (
   playerOneName = "Player One",
-  PlayerTwoName = "Player Two"
+  playerTwoName = "Player Two"
 ) {
   const players = [
     { name: playerOneName, mark: "X" },
-    { name: PlayerTwoName, mark: "O" },
+    { name: playerTwoName, mark: "O" },
   ];
 
   let activePlayer = players[0];
@@ -16,42 +16,50 @@ const playerManagement = (function (
         : (activePlayer = players[0]);
   };
 
+  const setActivePlayer = (num) => {
+    activePlayer = players[num];
+  };
+
   const getActivePlayerName = () => activePlayer.name;
 
   const getActivePlayerMark = () => activePlayer.mark;
 
-  return { switchPlayerTurn, getActivePlayerName, getActivePlayerMark };
+  return {
+    switchPlayerTurn,
+    setActivePlayer,
+    getActivePlayerName,
+    getActivePlayerMark,
+  };
 })();
 
-const GameBoard = (function () {
-  const board = ["", "", "", "", "", "", "", "", ""];
+const GameBoardModule = (function (player) {
+  let board = ["", "", "", "", "", "", "", "", ""];
 
   const getBoard = () => board;
 
   const addMark = (number) => {
-    const player = playerManagement;
-    const position = board[number];
+    if (board[number] !== "") return;
 
-    if (position != "") return;
-
-    const mark = player.getActivePlayerMark();
-
-    console.log(`Mark "${mark}" placed at position ${position}`);
+    board[number] = player.getActivePlayerMark();
   };
 
-  const printBoard = () => {
-    console.log(board);
+  const filledCells = () => {
+    return board.filter((cell) => {
+      return cell !== "";
+    }).length;
   };
 
-  return { getBoard, addMark, printBoard };
-})();
+  const resetBoard = () => {
+    board = ["", "", "", "", "", "", "", "", ""];
+    player.setActivePlayer[0];
+  };
 
-const GameControls = (function () {
-  const board = GameBoard;
-  boardCells = board.getBoard();
-  const player = playerManagement;
+  return { getBoard, addMark, filledCells, resetBoard };
+})(PlayerManager);
 
+const GameController = (function (board, player) {
   const checkWinner = () => {
+    const boardArray = board.getBoard();
     const winningConditions = [
       [0, 1, 2],
       [3, 4, 5],
@@ -62,13 +70,51 @@ const GameControls = (function () {
       [0, 4, 8],
       [6, 4, 2],
     ];
+
+    for (let condition of winningConditions) {
+      const [a, b, c] = condition;
+
+      if (
+        boardArray[a] &&
+        boardArray[a] === boardArray[b] &&
+        boardArray[a] === boardArray[c]
+      )
+        return true;
+    }
+
+    return false;
   };
+
   const playRound = (number) => {
+    if (board.getBoard()[number] !== "") return;
+
     console.log(`It is currently ${player.getActivePlayerName()}'s turn`);
 
     board.addMark(number);
-    board.printBoard();
+
+    console.log(
+      `Mark ${player.getActivePlayerMark()} has been placed at position ${number}`
+    );
+
+    console.log(`The board: ${board.getBoard()}`);
+
+    if (checkWinner() === true) {
+      console.log(
+        `We have a winner! ${player.getActivePlayerName()} wins! The game will now reset!`
+      );
+      board.resetBoard();
+      return;
+    }
+
+    if (checkWinner() === false && board.filledCells() >= 9) {
+      console.log(
+        "The game ended in a tie! Better luck next time! The game will now reset!"
+      );
+      board.resetBoard();
+      return;
+    }
+
     player.switchPlayerTurn();
   };
   return { checkWinner, playRound };
-})();
+})(GameBoardModule, PlayerManager);
